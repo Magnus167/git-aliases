5f4efe91dfd7c8a335a04f7189576a325c5754b3
Palash Tyagi <palash@tyagi.me>
2024-02-27 23:34:58 +0000

    Create branchc.sh

---
+++ aliases/branchc.sh
@@ -0,0 +1,9 @@
+# # branchc
+# ## Usage:
+# ```bash
+# git branchc
+# ```
+# ## Intended use:
+# Shorten the command to show the current branch.
+
+git config --global alias.branchc 'branch --show-current'
